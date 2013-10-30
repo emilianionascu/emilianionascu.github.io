abe3c8a405acf471f8f57b77a42acdca54eab173
Emilian Ionascu <emilian.ionascu@gmail.com>
2013-10-30 19:01:51 +0200

    Updated logo

---
+++ app/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Emilian Ionascu Desk
+ * Date: 10/30/13
+ * Time: 5:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+angular.module
